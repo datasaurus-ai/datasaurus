bb9b17a24ae24730e9fa1b6e799548738b69e5f5
Baptiste Colle <collebaptiste@gmail.com>
2023-10-28 09:39:27 +0200

    add seed data

---
+++ supabase/seed.sql
@@ -0,0 +1,11 @@
+-- Create the "pipeline" table
+CREATE TABLE IF NOT EXISTS pipeline (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    name TEXT NOT NULL,
+    prompt TEXT NOT NULL, 
+    endpoint TEXT NOT NULL
+);
+
+alter table pipeline
+  enable row level security;
